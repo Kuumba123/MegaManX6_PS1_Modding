66b68da6563e34172ac519b3a60fdcc33e84f111
PogChampGuy <Kuumba.Lambie@gmail.com>
2024-12-27 14:59:04 -0500

    added SetupScreen function

--- include/misc.h
+++ include/misc.h
@@ -8,5 +8,6 @@ uint GetReploidStatus(uint reploid); //2 = Rescued , 3 = Dead , 4 = Zombie
 void SetReploidStatus(uint8_t reploid,uint status);
 void SetupLayerSettings();
 void SetupPriority();
+void SetupScreen(uint size);
 void SetupSpawn();
 #endif
