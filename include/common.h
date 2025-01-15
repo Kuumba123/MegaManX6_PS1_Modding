8765a584b4d7a13ff4c4192bd7b84561074b3629
PogChampGuy <Kuumba.Lambie@gmail.com>
2025-01-14 19:44:36 -0500

    wrong function

--- include/common.h
+++ include/common.h
@@ -1001,7 +1001,7 @@ void FileCollect();
 void FileCollect2();
 
 void PlaySound(int slot,int id,void *objP);
-void TurnOffSounds();
+void TurnOffSounds(uint32_t flag, uint32_t file);
 void PlaySong(uint8_t id,uint8_t vol);
 
 char * strcpy(void * dest,void * src);
