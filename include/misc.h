01cfd6a757a1b6afd8a4fb31f46d07b08c123d53
PogChampGuy <Kuumba.Lambie@gmail.com>
2025-01-16 19:24:36 -0500

    added Text related functions

--- include/misc.h
+++ include/misc.h
@@ -4,8 +4,10 @@
 #include <stdbool.h>
 
 void LoadLevel();
-uint GetReploidStatus(uint32_t reploid); //2 = Rescued , 3 = Dead , 4 = Zombie
+uint32_t GetReploidStatus(uint32_t reploid); //2 = Rescued , 3 = Dead , 4 = Zombie
+bool HasSeenText(uint32_t id);
 void SetReploidStatus(uint8_t reploid,uint status);
+void SetSeenText(uint32_t id);
 void SetupLayerSettings();
 void SetupPriority();
 void SetupScreen(uint32_t size);
